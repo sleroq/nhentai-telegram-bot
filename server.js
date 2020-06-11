af8d82b6a8460eb2c56c829801d82d5f684f67df
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2020-06-11 17:13:56 +0300

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.telegram.setWebhook("https://nhentai-thing.glitch.me/secret-path");
+bot.telegram.setWebhook("https://kipeeng-alive.herokuapp.com/secret-path");
 
 const express = require("express");
 const expressApp = express();
