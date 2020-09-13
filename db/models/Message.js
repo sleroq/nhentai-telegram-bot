4c245959f74a200fa97f0fd2637d14fb43a8d82c
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2020-09-13 18:05:16 +0300

    rm old files

--- db/models/Message.js
+++
@@ -1,8 +0,0 @@
-const messageSchema = new Schema({
-  chat_id: { type: String, required: true },
-  message_id: { type: Number, required: true },
-  history: [Number],
-  date: { type: Date, default: Date.now },
-});
-
-module.exports = model("Message", messageSchema);
