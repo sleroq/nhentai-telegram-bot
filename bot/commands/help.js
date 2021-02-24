eb02592df56f5581ed88c1cb7e12b4b08c36d318
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-02-25 00:53:06 +0300

    Added github link to help message

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,6 +1,7 @@
 module.exports.help = async function (ctx) {
   await ctx.reply(ctx.i18n.t("help"), {
     parse_mode: "Markdown",
+    disable_web_page_preview: true,
     reply_markup: {
       inline_keyboard: [
         [
