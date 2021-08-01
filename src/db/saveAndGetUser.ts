cba43c0f7b6dc3e5aa18ffa8f9b916cd468b79bb
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-01 17:47:41 +0300

    refactoring

--- src/db/saveAndGetUser.ts
+++ src/db/saveAndGetUser.ts
@@ -1,15 +1,19 @@
-import User from "../models/user.model";
-import { awful_tags } from "../config.json";
-import config from "../../config.json";
+import User, { UserSchema } from "../models/user.model";
+import config, { awful_tags } from "../../config.json";
 import { Context } from "telegraf";
+import { Document } from "mongoose";
 
-module.exports.saveAndGetUser = async function (ctx: Context) {
+export default async function saveAndGetUser (ctx: Context): Promise<UserSchema & Document<any, any, UserSchema>|null> {
   if (!ctx.from) {
-    return;
+    return null;
   }
-  let user = await User.findById(ctx.from.id, function (err) {
-    if (err) console.log(err);
-  });
+  let user: UserSchema & Document<any, any, UserSchema> | null = null;
+  try {
+    user = await User.findById(ctx.from.id);
+  } catch (error){
+    console.log("Error when getting user");
+    console.log(error);
+  }
   if (!user) {
     user = new User({
       _id: ctx.from.id,
@@ -24,10 +28,13 @@ module.exports.saveAndGetUser = async function (ctx: Context) {
       can_repeat_in_random: config.can_repeat_in_random_by_default,
     })
 
-    await user.save(function (err) {
-      if (err) return console.error(err);
-      console.log("user saved");
-    })
+    try {
+      await user.save();
+    } catch (error){
+      console.log("Error saving user");
+      console.log(error);
+    }
+    console.log("New user saved!")
   }
   return user;
 };
