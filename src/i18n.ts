bf34d62a5a74311920605bf04a572e2927913c13
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-07 12:08:33 +0300

    Formatting with ESlint

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,10 @@
-import { I18n } from "i18n";
-import path from "path";
+import { I18n } from 'i18n'
+import path from 'path'
 
-const i18n = new I18n();
+const i18n = new I18n()
 i18n.configure({
-    locales: ["en", "es", "ru"],
-    defaultLocale: 'en',
-    directory: path.join(__dirname, "locales")
-});
-export default i18n;
+  locales:       ['en', 'es', 'ru'],
+  defaultLocale: 'en',
+  directory:     path.join(__dirname, 'locales'),
+})
+export default i18n
