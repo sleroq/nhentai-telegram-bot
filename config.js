f9e6db3de91e0f583a1fb0fbd2a486b27e241000
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-07 12:30:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,55 +1,54 @@
-{
-  "bot_username": "nhentai_searchbot",
-  "//comment0": "This will be used to sign telegra.ph pages",
+export default {
+  bot_username: 'nhentai_searchbot',
+  // This will be used to sign telegra.ph pages,
 
-  "api_enabled": true,
-  "//comment1": "show some stats as json on api/ page (details in: api.js, express.js)",
+  api_enabled: true,
+  // show some stats as json on api/ page (details in: api.js, express.js),
 
-  "donation_wallets": "",
-  "//comment2": "Add donation message to /help command with donation_wallets",
+  donation_wallets: '',
+  // Add donation message to /help command with donation_wallets,
 
-  "express_get_slash": "Hello, love <3",
-  "text_at_the_end_of_telegraph_page": "Thanks for reading this chapter!",
+  express_get_slash:                 'Hello, love <3',
+  text_at_the_end_of_telegraph_page: 'Thanks for reading this chapter!',
 
-  "pages_to_show_fix_button": 100,
-  "//comment3": "the number of pages in the manga required to add 'fix' button to message.",
+  pages_to_show_fix_button: 100,
+  // the number of pages in the manga required to add \'fix\' button to message.,
 
-  "maximum_codes_from_one_message": 30,
+  maximum_codes_from_one_message: 30,
 
-  "show_favorites_as_gallery": false,
-  "search_sorting_by_default": "date",
-  "//comment5": "date / popular",
+  show_favorites_as_gallery: false,
+  search_sorting_by_default: 'date',
+  // date / popular,
 
-  "search_appearance_by_default": "photo",
-  "//comment6": "article (same as gallery) / photo",
+  search_appearance_by_default: 'photo',
+  // article (same as gallery) / photo,
 
-  "random_localy_by_default": true,
-  "//comment7": "about random loacaly https://telegra.ph/Settings-04-09",
+  random_localy_by_default: true,
+  // about random loacaly https://telegra.ph/Settings-04-09,
 
-  "can_repeat_in_random_by_default": true,
+  can_repeat_in_random_by_default: true,
 
-  "like_button_false": "🖤",
-  "like_button_true": "♥️",
-  "check_mark": "✅",
+  like_button_false: '🖤',
+  like_button_true:  '♥️',
+  check_mark:        '✅',
 
-  "favorites_icon_inline": "https://i.imgur.com/TmxG1Qr.png",
-  "history_icon_inline": "https://i.imgur.com/vQxvN28.jpeg",
-  "next_page_icon_inline": "https://i.imgur.com/3AMTdoA.png",
-  "help_icon_inline": "https://i.imgur.com/j2zt4j7.png",
-  "sort_by_new_icon_inline": "https://i.imgur.com/j2zt4j7.png",
-  "sort_by_popular_icon_inline": "https://i.imgur.com/j2zt4j7.png",
+  favorites_icon_inline:       'https://i.imgur.com/TmxG1Qr.png',
+  history_icon_inline:         'https://i.imgur.com/vQxvN28.jpeg',
+  next_page_icon_inline:       'https://i.imgur.com/3AMTdoA.png',
+  help_icon_inline:            'https://i.imgur.com/j2zt4j7.png',
+  sort_by_new_icon_inline:     'https://i.imgur.com/j2zt4j7.png',
+  sort_by_popular_icon_inline: 'https://i.imgur.com/j2zt4j7.png',
 
-  "most_popular_tag": "big breasts",
-  "awful_tags": [
-    "rape",
-    "guro",
-    "scat",
-    "torture",
-    "moral degeneration",
-    "mind break",
-    "incest",
-    "futanari",
-    "shotacon",
-    "lolicon"
+  awful_tags: [
+    'rape',
+    'guro',
+    'scat',
+    'torture',
+    'moral degeneration',
+    'mind break',
+    'incest',
+    'futanari',
+    'shotacon',
+    'lolicon'
   ]
 }
