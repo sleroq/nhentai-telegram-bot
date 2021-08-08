d97a91bec5daf73bcf1e0a81b72572d1dacc3a6c
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-08 19:36:23 +0300

    Format with ESlint rules

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,12 @@
-import moment from "moment";
-import Manga, { MangaSchema } from "./models/manga.model";
-import User from "./models/user.model";
-import Message from "./models/message.model";
+import moment from 'moment'
+import Manga, { MangaSchema } from './models/manga.model'
+import User from './models/user.model'
+import Message from './models/message.model'
 
 function numberWithCommas(x: number): string {
-  var parts = x.toString().split(".");
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-  return parts.join(",");
+  const parts = x.toString().split('.')
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+  return parts.join(',')
 }
 
 interface Count {
@@ -17,64 +17,64 @@ interface Count {
 
 export default class api {
   static async countManga(): Promise<Count> {
-    const count = await Manga.countDocuments({});
+    const count = await Manga.countDocuments({})
     return {
       count: { number: count, string: numberWithCommas(count) }
     }
   }
   static async countUsers(): Promise<Count> {
-    const count = await User.countDocuments({});
+    const count = await User.countDocuments({})
     return {
       count: { number: count, string: numberWithCommas(count) }
     }
   }
   static async countMessages(): Promise<Count> {
-    const count = await Message.countDocuments({});
+    const count = await Message.countDocuments({})
     return {
       count: { number: count, string: numberWithCommas(count) }
     }
   }
   static async messagesToday(): Promise<Count> {
-    const date_now = moment().toDate();
-    const date_tomorrow = moment().add(1, "d").toDate();
+    const date_now = moment().toDate()
+    const date_tomorrow = moment().add(1, 'd').toDate()
     const count = await Message.countDocuments({
       updatedAt: {
         $gte: date_now,
-        $lt: date_tomorrow
+        $lt:  date_tomorrow
       }
-    });
+    })
     return {
       count: { number: count, string: numberWithCommas(count) }
     }
   }
   static async usersToday(): Promise<Count> {
-    const date_now = moment().toDate();
-    const date_tomorrow = moment().add(1, "d").toDate();
+    const date_now = moment().toDate()
+    const date_tomorrow = moment().add(1, 'd').toDate()
     const count = await User.countDocuments({
       createdAt: {
         $gte: date_now,
-        $lt: date_tomorrow
+        $lt:  date_tomorrow
       }
-    });
+    })
     return {
       count: { number: count, string: numberWithCommas(count) }
     }
   }
   static async mangaToday(): Promise<Count> {
-    const date_now = moment().toDate();
-    const date_tomorrow = moment().add(1, "d").toDate();
+    const date_now = moment().toDate()
+    const date_tomorrow = moment().add(1, 'd').toDate()
     const count = await Manga.countDocuments({
       createdAt: {
         $gte: date_now,
-        $lt: date_tomorrow
+        $lt:  date_tomorrow
       }
-    });
+    })
     return {
       count: { number: count, string: numberWithCommas(count) }
     }
   }
   static async lastManga(): Promise<{ manga: MangaSchema }|void> {
-    const manga = await Manga.findOne({}, {}, { sort: { _id: -1 } });
+    const manga = await Manga.findOne({}, {}, { sort: { _id: -1 } })
     if(manga){
       return {
         manga: manga
@@ -87,16 +87,16 @@ export default class api {
     const messagesTotal = await this.countMessages()
     const messagesToday = await this.messagesToday()
     const usersToday = await this.usersToday()
-    const mangaToday = await this.mangaToday();
-    const lastManga = await this.lastManga();
+    const mangaToday = await this.mangaToday()
+    const lastManga = await this.lastManga()
     return {
-      users_total: users.count,
-      manga_total: doujins.count,
+      users_total:    users.count,
+      manga_total:    doujins.count,
       messages_total: messagesTotal.count,
       messages_today: messagesToday.count,
-      manga_today: mangaToday.count,
-      users_today: usersToday.count,
-      last_manga: lastManga,
+      manga_today:    mangaToday.count,
+      users_today:    usersToday.count,
+      last_manga:     lastManga,
     }
   }
 }
