7cfb1ea8e348d805fc4770c551b23830bc872a2f
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-08 19:51:23 +0300

    Update i18n.ts
    (still does not work)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,17 @@
 import { I18n } from 'i18n'
-import path from 'path'
+// import path from 'path'
 
 const i18n = new I18n()
 i18n.configure({
   locales:       ['en', 'es', 'ru'],
   defaultLocale: 'en',
-  directory:     path.join(__dirname, 'locales'),
+  staticCatalog: {
+    en: require('./locales/en'),
+    ru: require('./locales/ru'),
+    es: require('./locales/es'),
+  }
+  // directory:     path.join(__dirname, 'locales'),
 })
+// console.log(i18n.__('greeting', 'ru'))
+// console.log(path.join(__dirname, 'locales'))
 export default i18n
