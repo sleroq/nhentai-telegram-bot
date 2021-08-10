79b9ca44112d6878fac1ff126e76ae491aed92e2
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-10 23:22:30 +0300

    Added 'previous' button handling

--- src/bot/callback_handler.ts
+++ src/bot/callback_handler.ts
@@ -1,19 +1,11 @@
-import saveAndGetUser from '../db/save_and_get_user'
-import Verror from 'verror'
 import {Context} from 'telegraf'
 import {Update} from 'telegraf/typings/core/types/typegram'
-import {UserSchema} from '../models/user.model'
-import { Document } from 'mongoose'
 import makeRandom from './commands/random'
 
 export default async function callbackHandler(ctx: Context<Update>, query: string): Promise<void> {
-  let user: UserSchema & Document<any, any, UserSchema> | undefined
-  try {
-    user = await saveAndGetUser(ctx)
-  } catch (error) {
-    throw new Verror(error, 'Getting user in callbackHandler')
-  }
   if (query === 'r'){
-    await makeRandom(ctx, user)
+    await makeRandom(ctx,'next')
+  } else if (query === 'previous'){
+    await makeRandom(ctx,'previous')
   }
 }
