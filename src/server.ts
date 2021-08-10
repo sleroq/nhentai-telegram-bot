8ca6de09f3e9baf35c525d3413ec01318440df5e
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-10 23:40:43 +0300

    Uncomment what works*
    * - probably works

--- src/server.ts
+++ src/server.ts
@@ -9,14 +9,14 @@ import startWithWebhook from './express'
 import saveAndGetUser from './db/save_and_get_user'
 import i18n from './i18n'
 // Import all commands
-// import randomCommand from './bot/commands/random.js'
 // import dlzip from './bot/commands/dlzip.js'
 // import help from './bot/commands/help.js'
 // import settings from './bot/settings/settings.js'
 
-// import cb_query from './bot/buttons/index.js'
+import callbackHandler from './bot/callback_handler'
+import makeRandom from './bot/commands/random'
+import textHandler from './bot/text_handler'
 import inlineSearch from './bot/inline_search/index'
-// import textHandler from './bot/commands/textHandler.js'
 
 let token: string | undefined
 if (!process.env.BOT_TOKEN) {
@@ -30,7 +30,7 @@ if (!process.env.DATABASE_URL) {
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-//coonnect to the database
+// Connect to the database
 import mongoose from 'mongoose'
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -41,7 +41,7 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () { console.log('mongoose is connected!') })
 
-// commands that always work (without nhentai/telegraph connections)
+// Commands that always work (without nhentai/telegraph connections)
 
 bot.start(async (ctx) => {
   await saveAndGetUser(ctx)
@@ -83,20 +83,46 @@ bot.command('id', async (ctx) => {
 })
 
 // // commands with nhentai
-// bot.command("rand", async (ctx) => { await randomCommand(ctx); });
+bot.command('rand', async (ctx) => {
+  try {
+    await makeRandom(ctx, 'next')
+  } catch (error) {
+    console.error('Random text command: ' + error)
+  }
+})
 // bot.command("zip", async (ctx) => { await dlzip(ctx); });
-// // non-text
-// bot.on("callback_query", async (ctx) => { await cb_query(ctx); });
-bot.on('inline_query', async (ctx) => { 
+
+// non-text
+bot.on('callback_query', async (ctx) => {
+  if (!('data' in ctx.update.callback_query)
+    || !ctx.update.callback_query.data
+    || !ctx.from) {
+    return
+  }
+  const query = ctx.update.callback_query.data
+  try {
+    await callbackHandler(ctx, query)
+  } catch (error) {
+    console.error('Callback query: ', error)
+  }
+})
+
+bot.on('inline_query', async (ctx) => {
   try { 
     await inlineSearch(ctx) 
   } catch (error) {
     console.error('Inline search: ', error)
   } 
-});
+})
 
-// // get with id
-// bot.on("text", async (ctx) => { await textHandler(ctx); });
+// get with id
+bot.on('text', async (ctx) => {
+  try {
+    await textHandler(ctx)
+  } catch (error) {
+    console.error('textHandler: ' + error)
+  }
+});
 
 // start the bot 
 (async()=>{
