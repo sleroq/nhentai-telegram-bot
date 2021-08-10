c8d56e9445b76811d844d06741b5a648769204f8
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-10 23:41:35 +0300

    Added openInTelegraph

--- src/bot/callback_handler.ts
+++ src/bot/callback_handler.ts
@@ -1,11 +1,14 @@
 import {Context} from 'telegraf'
 import {Update} from 'telegraf/typings/core/types/typegram'
 import makeRandom from './commands/random'
+import openIInTelegraph from './commands/open_in_telegraph'
 
 export default async function callbackHandler(ctx: Context<Update>, query: string): Promise<void> {
   if (query === 'r'){
     await makeRandom(ctx,'next')
   } else if (query === 'previous'){
     await makeRandom(ctx,'previous')
+  } else if (query.match('open')) {
+    await openIInTelegraph(ctx)
   }
 }
