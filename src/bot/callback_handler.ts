207e58d2a3cabcfc799df66609a87394f001c3d9
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-11 22:36:46 +0300

    Added try catch for better error handling

--- src/bot/callback_handler.ts
+++ src/bot/callback_handler.ts
@@ -1,14 +1,35 @@
 import {Context} from 'telegraf'
 import {Update} from 'telegraf/typings/core/types/typegram'
 import makeRandom from './commands/random'
-import openIInTelegraph from './commands/open_in_telegraph'
+import openInTelegraph from './commands/open_in_telegraph'
+import likeDoujin from './commands/like'
+import Verror from 'verror'
 
 export default async function callbackHandler(ctx: Context<Update>, query: string): Promise<void> {
+  console.log(query)
   if (query === 'r'){
-    await makeRandom(ctx,'next')
+    try {
+      await makeRandom(ctx,'next')
+    } catch (error) {
+      throw new Verror(error, 'Random - \'next\' button')
+    }
   } else if (query === 'previous'){
-    await makeRandom(ctx,'previous')
+    try {
+      await makeRandom(ctx,'previous')
+    } catch (error) {
+      throw new Verror(error, 'Random - \'previous\' button')
+    }
   } else if (query.match('open')) {
-    await openIInTelegraph(ctx)
+    try {
+      await openInTelegraph(ctx)
+    } catch (error) {
+      throw new Verror(error, 'openInTelegraph')
+    }
+  } else if (query.match('like_')){
+    try {
+      await likeDoujin(ctx)
+    } catch (error) {
+      throw new Verror(error, 'likeDoujin')
+    }
   }
 }
