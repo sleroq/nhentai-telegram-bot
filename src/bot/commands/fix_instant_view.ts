48de4dac3e071922005a98a971248b50fe73bb34
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-17 22:18:51 +0300

    Refactoring
    Now should at least work

--- src/bot/commands/fix_instant_view.ts
+++ src/bot/commands/fix_instant_view.ts
@@ -1,19 +1,24 @@
-import nhentai, {Doujin} from '../../lib/nhentai'
-import {Context} from 'telegraf'
-import {UserSchema} from '../../models/user.model'
-import {Document} from 'mongoose'
-import saveAndGetUser from '../../db/save_and_get_user'
-import Verror from 'verror'
-import saveAndGetManga from '../../db/save_and_get_manga'
-import {MangaSchema} from '../../models/manga.model'
-import {InlineKeyboardButton} from 'typegram'
-import i18n from '../../lib/i18n'
-import {CallbackQuery} from 'telegraf/typings/core/types/typegram'
-import Message, {MessageSchema} from '../../models/message.model'
-import { uploadByUrl } from 'telegraph-uploader'
-import {telegraphCreatePage} from '../../lib/telegraph'
+import Verror                   from 'verror'
+import nhentai, {Doujin}        from '../../lib/nhentai'
+import {
+  uploadByUrl,
+  uploadResult
+}                               from 'telegraph-uploader'
+import { telegraphCreatePage }  from '../../lib/telegraph'
+import i18n                     from '../../lib/i18n'
+import config                   from '../../../config'
+import { getMangaMessage }      from '../../lib/some_functions'
+import saveAndGetUser           from '../../db/save_and_get_user'
+import saveAndGetManga          from '../../db/save_and_get_manga'
+import Message                  from '../../models/message.model'
+import { UserSchema }           from '../../models/user.model'
+import { MangaSchema }          from '../../models/manga.model'
+import { Document }             from 'mongoose'
+import { Context }              from 'telegraf'
+import { InlineKeyboardButton } from 'typegram'
+import { CallbackQuery }        from 'telegraf/typings/core/types/typegram'
 
-export default async function fixInstantView(ctx: Context, callback_query: CallbackQuery.DataCallbackQuery) {
+export default async function fixInstantView(ctx: Context, callback_query: CallbackQuery.DataCallbackQuery): Promise<void> {
 
   const matchId = callback_query.data.match(/_[0-9]+/)
   if(!matchId || !Number(matchId[0])) {
@@ -34,7 +39,7 @@ export default async function fixInstantView(ctx: Context, callback_query: Callb
   } catch (error) {
     if(error.message === 'Not found') {
       try {
-        await ctx.reply(i18n.__('manga_does_not_exist') + '\n(' + mangaId + ')')
+        await ctx.reply(i18n.__('manga_does_not_exist') + '\n(' + doujinId + ')')
       } catch (error) {
         console.error('Replying \'404\'' + error)
       }
@@ -45,23 +50,18 @@ export default async function fixInstantView(ctx: Context, callback_query: Callb
   let fixedUrl = doujin.telegraph_fixed_url
 
   if (!fixedUrl) {
-    let doujinWithPages: Doujin
-    try {
-      doujinWithPages = await nhentai.getDoujin(doujin.id)
-    } catch (error) {
-      if (error.message === 'Not found') {
-        try {
-          await ctx.reply(i18n.__('cant_get_anymore'))
-        } catch (error) {
-          throw new Verror(error, 'Replying \'404\'')
-        }
-        return
+    const pages = await getPages(doujin.id)
+  
+    if (pages === 404) {
+      try {
+        await ctx.reply(i18n.__('cant_get_anymore'))
+      } catch (error) {
+        throw new Verror(error, 'Replying \'404\'')
       }
-      throw new Verror(error, 'Getting doujin to fix pages')
+      return
     }
-    const pages = doujinWithPages.pages
-
-    let attemptsCnt = 0    // coun't retries because err
+  
+    let attemptsCnt = 0    // count retries because err
     let telegraphUrls: string[] = []
     // in case it isn't the first try to fix manga we dont want to reupload same pages
     if (Array.isArray(doujin.fixed_pages) && doujin.fixed_pages.length) {
@@ -73,56 +73,28 @@ export default async function fixInstantView(ctx: Context, callback_query: Callb
 
     for (let i = telegraphUrls.length || 0; i < pages.length; i++) {
       const fixing_keyboard: InlineKeyboardButton[][] = [[]]
-
+      const telegraph_url = doujin.telegraph_url
       // while manga is fixing you can still try to open broken one:
-      if(doujin.telegraph_url){
+      if (telegraph_url) {
         fixing_keyboard[0].push({
           text: 'Telegra.ph',
-          url:  doujin.telegraph_url,
+          url:  telegraph_url,
         })
       }
-      // incase we were retrying after err 3 times - stop it
+
+      // in case we were retrying after err 3 times - stop it
       if (attemptsCnt > 2) {
-        fixing_keyboard[0].unshift({
-          // button to try again:
-          text:          i18n.__('try_again_later'),
-          callback_data: 'fixLater_' + doujin.id + '_' + new Date(),
-        })
-        // in case it happen not in inline search we should add buttons back:
-        if (callback_query.message) {
-          fixing_keyboard.push([
-            {
-              text: i18n.__('search_button'),
-              switch_inline_query_current_chat: '',
-            },
-          ])
-          fixing_keyboard.push([
-            {
-              text:          i18n.__('next_button'),
-              callback_data: 'r',
-            },
-          ])
-          const message_db = await Message.findOne({
-            message_id: callback_query.message.message_id,
-            chat_id: String(callback_query.message.from?.id),
-          })
-          if (message_db && message_db.current > 0) {
-            fixing_keyboard[2].unshift({
-              text:          i18n.__('previous_button'),
-              callback_data: 'previous',
-            })
-          }
-        }
+        const fixingKeyboardBack: InlineKeyboardButton[][] = await buildKeyboardBack(telegraph_url, doujin.id, callback_query)
         try {
           await ctx.editMessageReplyMarkup({
-            inline_keyboard: fixing_keyboard,
+            inline_keyboard: fixingKeyboardBack,
           })
         } catch (error) {
           console.error('editMessageReplyMarkup while fixing pages stopped trying: ', error)
         }
         return
       }
-      let newUrl: { link: string, path: string } | undefined
+      let newUrl: uploadResult | undefined
       try {
         newUrl = await uploadByUrl(pages[i])
       } catch (error) {
@@ -134,12 +106,11 @@ export default async function fixInstantView(ctx: Context, callback_query: Callb
         )
         i -= 1
         attemptsCnt += 1
+
         fixing_keyboard[0].unshift({
-          text:          'flood wait err',
+          text:          'flood wait, waiting 10 seconds',
           callback_data: 'flood_wait',
         })
-
-        await sleep(5000) // idk if it helps
       }
 
       if (newUrl && newUrl.link) {
@@ -154,106 +125,142 @@ export default async function fixInstantView(ctx: Context, callback_query: Callb
         callback_data: 'fixing',
       })
       try {
-        await ctx
-          .editMessageReplyMarkup({
-            inline_keyboard: fixing_keyboard,
-          })
+        await ctx.editMessageReplyMarkup({
+          inline_keyboard: fixing_keyboard,
+        })
       } catch (error) {
         console.error('editMessageReplyMarkup while fixing pages: ', error)
       }
+      await sleep(5000) // maybe with help
     }
 
     try {
       fixedUrl = (await telegraphCreatePage(doujin, telegraphUrls)).url
-
     } catch (error) {
       console.error('Creating fixed telegraph page', error)
     }
 
-    const keyboard: InlineKeyboardButton[][] = []
-    // if there is no url somehow, prevent err, and allow user to return
-    if (!fixedUrl) {
-      keyboard[0].push({
-        text: 'Telegra.ph',
-        url:  telegraphUrl,
-      })
-      if (callback_query.message) {
-        keyboard[0].unshift({
-          text:          i18n.__('previous_button'),
-          callback_data: 'previous',
-        })
-      }
-      await ctx
-        .editMessageReplyMarkup({
-          inline_keyboard: keyboard,
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-      return
+    // save new url of course:
+    doujin.telegraph_fixed_url = fixedUrl
+    // delete all fixed pages cause we already have webpage
+    doujin.fixed_pages = []
+    try {
+      await doujin.save()
+    } catch (error) {
+      console.error('saving fixed doujin: ', error)
     }
   }
-  // save new url ofcourse:
-  manga_db.telegraph_fixed_url = telegraph_fixed_url;
-  // delete all fixed pages cause we already have webpage
-  manga_db.fixed_pages = [];
-  manga_db.save();
 
-  let heart = user.favorites.id(manga.id) ? config.like_button_true : config.like_button_false,
-    messageText = getMangaMessage(manga, telegraph_fixed_url, ctx.i18n),
-    inline_keyboard = [
-      [
-        {
-          text: "Telegra.ph",
-          url: telegraph_fixed_url,
-        },
-        { text: heart, callback_data: "like_" + manga.id },
-      ],
-    ];
-  if (ctx.update.callback_query.message) {
+  const heart = user.favorites.includes(doujin.id) ? config.like_button_true : config.like_button_false
+  const messageText = getMangaMessage(doujin, fixedUrl)
+  const inline_keyboard: InlineKeyboardButton[][] = [
+    [
+      {
+        text: 'Telegra.ph',
+        url:  String(fixedUrl),
+      },
+      {text: heart, callback_data: 'like_' + doujin.id},
+    ],
+  ]
+  if (callback_query.message) {
     inline_keyboard.push([
       {
-        text: ctx.i18n.t("search_button"),
-        switch_inline_query_current_chat: "",
+        text: i18n.__('search_button'),
+        switch_inline_query_current_chat: '',
       },
-    ]);
+    ])
     inline_keyboard.push([
-      { text: ctx.i18n.t("next_button"), callback_data: "r_prev" + manga.id },
-    ]);
-    let message = await Message.findOne({
-      message_id: ctx.update.callback_query.message.message_id,
-      chat_id: ctx.update.callback_query.message.from.id,
-    });
+      {
+        text: i18n.__('next_button'),
+        callback_data: 'r_prev' + doujin.id
+      },
+    ])
+
+    const message = await Message.findOne({
+      message_id: callback_query.message.message_id,
+      chat_id: String(callback_query.message.from?.id),
+    })
     if (message && message.current > 0) {
       inline_keyboard[2].unshift({
-        text: ctx.i18n.t("previous_button"),
-        callback_data: "prev_" + manga.id,
-      });
+        text: i18n.__('previous_button'),
+        callback_data: 'prev_' + doujin.id,
+      })
     }
   }
-
-  console.log(
-    "fixed from this - " + telegraph_url + "\nto this - " + telegraph_fixed_url
-  );
-
-  await ctx
-    .editMessageText(messageText, {
-      parse_mode: "HTML",
+  console.log('fixed pages! new url: ' + fixedUrl)
+  try {
+    await ctx.editMessageText(messageText, {
+      parse_mode: 'HTML',
       reply_markup: {
         inline_keyboard: inline_keyboard,
       },
     })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+  } catch (error) {
+    console.error('editing message with fixed doujin: ', error)
+  }
+}
 
-function uniq(a) {
-  var seen = {};
+function uniq(a: string[]) {
+  const seen: Record<string, boolean> = {}
   return a.filter(function (item) {
-    return seen.hasOwnProperty(item) ? false : (seen[item] = true);
-  });
+    return Object.prototype.hasOwnProperty.call(seen, item) ? false : (seen[item] = true)
+  })
 }
 function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+async function getPages(id: number | string): Promise<string[] | 404> {
+  let doujinWithPages: Doujin
+  try {
+    doujinWithPages = await nhentai.getDoujin(id)
+  } catch (error) {
+    if (error.message === 'Not found') {
+      return 404
+    }
+    throw new Verror(error, 'Getting doujin to fix pages')
+  }
+  return doujinWithPages.pages
+}
+
+async function buildKeyboardBack(telegraph_url: string | undefined, id: string, callback_query: CallbackQuery.DataCallbackQuery): Promise<InlineKeyboardButton[][]> {
+  const fixing_keyboard: InlineKeyboardButton[][] = [[]]
+
+  // while manga is fixing you can still try to open broken one:
+  if (telegraph_url) {
+    fixing_keyboard[0].push({
+      text: 'Telegra.ph',
+      url:  telegraph_url,
+    })
+  }
+  fixing_keyboard[0].unshift({
+    // button to try again:
+    text:          i18n.__('try_again_later'),
+    callback_data: 'fixLater_' + id + '_' + new Date(),
+  })
+  // in case it happen not in inline search we should add buttons back:
+  if (callback_query.message) {
+    fixing_keyboard.push([
+      {
+        text: i18n.__('search_button'),
+        switch_inline_query_current_chat: '',
+      },
+    ])
+    fixing_keyboard.push([
+      {
+        text: i18n.__('next_button'),
+        callback_data: 'r',
+      },
+    ])
+    const message_db = await Message.findOne({
+      message_id: callback_query.message.message_id,
+      chat_id:    String(callback_query.message.from?.id),
+    })
+    if (message_db && message_db.current > 0) {
+      fixing_keyboard[2].unshift({
+        text:          i18n.__('previous_button'),
+        callback_data: 'previous',
+      })
+    }
+  }
+  return fixing_keyboard
 }
