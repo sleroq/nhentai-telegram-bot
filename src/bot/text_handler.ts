d7999c06ca13e0bbc94ead4fc67247a8aa99b6e7
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-22 23:47:11 +0300

    Update text_handler.ts
    "Fixed" chat_id and message_id for messages
    Getting keyboard with assembleKeyboard function now

--- src/bot/text_handler.ts
+++ src/bot/text_handler.ts
@@ -1,14 +1,18 @@
-import {Context} from 'telegraf'
 import config from '../../config'
-import saveAndGetManga from '../db/save_and_get_manga'
-import {MangaSchema} from '../models/manga.model'
-import {Document} from 'mongoose'
+import i18n from '../lib/i18n'
 import Verror from 'verror'
-import {UserSchema} from '../models/user.model'
-import saveAndGetUser from '../db/save_and_get_user'
-import {getMangaMessage, isFullColor} from '../lib/some_functions'
-import Message from '../models/message.model'
-import {InlineKeyboardButton} from 'typegram'
+
+import {
+  assembleKeyboard,
+  getMangaMessage
+} from '../lib/some_functions'
+import saveAndGetManga from '../db/save_and_get_manga'
+import saveAndGetUser  from '../db/save_and_get_user'
+
+import Message   from '../models/message.model'
+import { Manga } from '../models/manga.model'
+import { User }  from '../models/user.model'
+import { Context }              from 'telegraf'
 
 // TODO: be able to work without database connection
 export default async function textHandler(ctx: Context): Promise<void> {
@@ -24,7 +28,7 @@ export default async function textHandler(ctx: Context): Promise<void> {
   ) {
     return
   }
-  let user: UserSchema & Document<any, any, UserSchema> | undefined
+  let user: User | undefined
   try {
     user = await saveAndGetUser(ctx)
   } catch (error) {
@@ -34,29 +38,30 @@ export default async function textHandler(ctx: Context): Promise<void> {
   const messageText = ctx.message.text
   // find numbers, remove duplicates:
   const ids = Array.from(new Set(messageText.match(/\d+/gm)))
-  if (!ids || !ids[0]) {
+  if (!ids[0]) {
     return
   }
   for (const id of ids) {
-    if (ids.indexOf(id) > config.maximum_codes_from_one_message) {
+    const index = ids.indexOf(id)
+    if (index > config.maximum_codes_from_one_message) {
       console.log('textHandler: Stop: Reached limit' + config.maximum_codes_from_one_message + ' codes')
       return
     }
     console.log('textHandler started work on ' + id)
 
-    let manga: MangaSchema & Document<any, any, MangaSchema> | undefined
+    let manga: Manga| undefined
     try {
       manga = await saveAndGetManga(user, Number(id))
     } catch (error) {
       if(error.message === 'Not found') {
         try {
-          await ctx.reply(i18n.__('manga_does_not_exist') + '\n(' + id + ')')
+          await ctx.reply(i18n.t('manga_does_not_exist') + '\n(' + id + ')')
         } catch (error) {
           console.error('Replying \'404\'' + error)
         }
       } else {
         try {
-          await ctx.reply(i18n.__('failed_to_get') + '\n(`' + id + '`)', {
+          await ctx.reply(i18n.t('failed_to_get') + '\n(`' + id + '`)', {
             parse_mode: 'HTML',
           })
         } catch (error) {
@@ -71,45 +76,13 @@ export default async function textHandler(ctx: Context): Promise<void> {
       : manga.telegraph_url
 
     const message = new Message({
-      chat_id:    ctx.message.from.id,
-      message_id: ctx.message.message_id,
+      chat_id:    ctx.message.chat.id,
+      message_id: ctx.message.message_id + 1 + index,
       current:    0,
       history:    [],
     })
     const messageText = getMangaMessage(manga, telegraph_url)
-    const heart = user.favorites.includes(manga.id) ? '♥️' : '🖤'
-    const inlineKeyboard: InlineKeyboardButton[][] = [
-      [
-        {
-          text: 'Telegra.ph',
-          url:  String(telegraph_url)
-        },
-        {
-          text:          heart,
-          callback_data: 'like_' + manga.id
-        },
-      ],
-      [
-        {
-          text: i18n.__('search_button'),
-          switch_inline_query_current_chat: '',
-        },
-      ],
-      [
-        {
-          text:          i18n.__('next_button'),
-          callback_data: 'r'
-        }
-      ],
-    ]
-    const numberOfPages = manga.pages
-
-    if (!manga.telegraph_fixed_url && (numberOfPages > config.pages_to_show_fix_button || isFullColor(manga))) {
-      inlineKeyboard[0].unshift({
-        text:          i18n.__('fix_button'),
-        callback_data: 'fix_' + manga.id,
-      })
-    }
+    const inlineKeyboard = assembleKeyboard(user, manga, telegraph_url)
     message.history.push(manga.id)
     user.manga_history.push(manga.id)
     try {
