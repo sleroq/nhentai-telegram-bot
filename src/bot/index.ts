95efec189935e797516f5e0c62f8361d78a79924
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-22 23:40:50 +0300

    Move bot initialization to a separate file

---
+++ src/bot/index.ts
@@ -0,0 +1,122 @@
+import { Telegraf } from 'telegraf'
+import Verror       from 'verror'
+
+import saveAndGetUser   from '../db/save_and_get_user'
+import i18n             from '../lib/i18n'
+
+// Import all commands
+import callbackHandler from './callback_handler'
+import makeRandom      from './commands/random'
+import textHandler     from './text_handler'
+import inlineSearch    from './inline_search/index'
+import help            from './commands/help'
+import settings        from './commands/settings/settings'
+
+const botToken = process.env.BOT_TOKEN
+if (!botToken) {
+  throw new Error('No BOT_TOKEN in env')
+}
+const bot = new Telegraf(botToken)
+bot.catch((error)=>{
+  console.error(error)
+})
+
+// Commands that always work (without nhentai/telegraph connections)
+bot.start(async (ctx) => {
+  await saveAndGetUser(ctx)
+  const message = i18n.t('greeting', { name: ctx.from.first_name})
+  try {
+    await ctx.reply(message, {
+      parse_mode:   'HTML',
+      reply_markup: {
+        inline_keyboard: [
+          [{
+            text:          i18n.t('random_button'),
+            callback_data: 'r'
+          }],
+        ],
+      },
+    })
+  } catch (error) {
+    throw new Verror(error, 'Replying greetings')
+  }
+})
+
+bot.help(async (ctx) => {
+  try {
+    await help(ctx)
+  } catch (error) {
+    throw new Verror('Handling \'/help\' command')
+  }
+})
+bot.command('code', async (ctx) => {
+  try {
+    await ctx.reply('Just send me a code')
+  } catch (error) {
+    throw new Verror(error, 'Replying on \'/code\' command')
+  }
+})
+
+bot.command('settings', async (ctx) => {
+  try {
+    await settings(ctx)
+  } catch (error) {
+    throw new Verror(error, 'Handling settings')
+  }
+})
+
+bot.command('id', async (ctx) => {
+  try {
+    await ctx.reply('`' + ctx.from.id + '`', { parse_mode: 'Markdown' })
+  } catch (error) {
+    throw new Verror(error, 'Replying on \'/id\' command')
+  }
+})
+// commands with nhentai
+bot.command('rand', async (ctx) => {
+  try {
+    await makeRandom(ctx, 'next')
+  } catch (error) {
+    throw new Verror(error, 'Handling \'/rand\' command')
+  }
+})
+
+bot.command('zip', async (ctx) => {
+  try {
+    await ctx.reply('This does not work in this version yet')
+  } catch (error) {
+    throw new Verror(error, 'Replying on \'/zip\' command')
+  }
+})
+
+bot.on('callback_query', async (ctx) => {
+  if (!('data' in ctx.update.callback_query)
+    || !ctx.update.callback_query.data
+    || !ctx.from) {
+    return
+  }
+  const callback_query = ctx.update.callback_query
+  try {
+    await callbackHandler(ctx, callback_query)
+  } catch (error) {
+    throw new Verror(error, 'Handling callback_query')
+  }
+})
+
+bot.on('inline_query', async (ctx) => {
+  try {
+    await inlineSearch(ctx)
+  } catch (error) {
+    throw new Verror(error, 'Handling inline_query')
+  }
+})
+
+bot.on('text', async (ctx) => {
+  try {
+    await textHandler(ctx)
+  } catch (error) {
+    throw new Verror(error, 'Handling text')
+  }
+})
+
+export default bot
--- src/server.ts
+++ src/server.ts
@@ -1,122 +1,36 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import { Telegraf } from 'telegraf'
-import Verror       from 'verror'
+import connectToMongo    from './db/connect'
+import { createAccount } from './lib/telegraph'
+import bot               from './bot/index'
+import startWithWebhook  from './express'
 
-import startWithWebhook from './express'
-import saveAndGetUser   from './db/save_and_get_user'
-import i18n             from './lib/i18n'
+import Verror from 'verror'
 
-// Import all commands
-import callbackHandler from './bot/callback_handler'
-import makeRandom      from './bot/commands/random'
-import textHandler     from './bot/text_handler'
-import inlineSearch    from './bot/inline_search/index'
-import connectToMongo  from './db/connect'
-
-const token = process.env.BOT_TOKEN
-if (!token) {
-  throw new Verror('No BOT_TOKEN in .env')
-}
-
-await connectToMongo()
-const bot = new Telegraf(token)
-
-// Connect to the database
-
-
-
-// Commands that always work (without nhentai/telegraph connections)
-bot.start(async (ctx) => {
-  await saveAndGetUser(ctx)
-  const message = i18n.__('greeting')
-  try {
-    ctx.reply(message, {
-      parse_mode:   'HTML',
-      reply_markup: {
-        inline_keyboard: [
-          [{
-            text:          i18n.__('random_button'),
-            callback_data: 'r'
-          }],
-        ],
-      },
-    })
-  } catch (err) {
-    console.log(err)
+(async() => {
+  // create telegra.ph token
+  if (!process.env.TELEGRAPH_TOKEN) {
+    process.env.TELEGRAPH_TOKEN = await createAccount()
   }
-})
 
-// bot.help(async (ctx) => {
-//   await help(ctx)
-// })
-bot.command('code', async (ctx) => {
-  try {
-    await ctx.reply('Just send me a code')
-  } catch (err) {
-    return
-  }
-})
-// bot.command('settings', async (ctx) => { await settings(ctx) })
-bot.command('id', async (ctx) => {
-  try {
-    await ctx.reply('`' + ctx.from.id + '`', { parse_mode: 'Markdown' })
-  } catch (err) {
-    return
-  }
-})
+  // Connect to the mongo database
+  await connectToMongo()
 
-// // commands with nhentai
-bot.command('rand', async (ctx) => {
-  try {
-    await makeRandom(ctx, 'next')
-  } catch (error) {
-    console.error('Random text command: ', error)
+  // start the bot
+  const botToken = process.env.BOT_TOKEN
+  if (!botToken) {
+    throw new Error('No BOT_TOKEN in env')
   }
-})
-// bot.command("zip", async (ctx) => { await dlzip(ctx); });
-
-
-bot.on('callback_query', async (ctx) => {
-  if (!('data' in ctx.update.callback_query)
-    || !ctx.update.callback_query.data
-    || !ctx.from) {
-    return
+  if (process.env.REPL_URL || process.env.HEROKU_URL) {
+    try {
+      await startWithWebhook(bot, botToken)
+    } catch (error) {
+      throw new Verror(error, 'Starting bot with webhook')
+    }
+    console.log('Bot is started webhook!')
+  } else {
+    await bot.launch({ dropPendingUpdates: true })
+    console.log('Bot is started polling!')
   }
-  const callback_query = ctx.update.callback_query
-  try {
-    await callbackHandler(ctx, callback_query)
-  } catch (error) {
-    console.error('Callback query: ', error)
-  }
-})
-
-bot.on('inline_query', async (ctx) => {
-  try { 
-    await inlineSearch(ctx) 
-  } catch (error) {
-    console.error('Inline search: ', error)
-  } 
-})
-
-bot.on('text', async (ctx) => {
-  try {
-    await textHandler(ctx)
-  } catch (error) {
-    console.error('textHandler: ', error)
-  }
-})
-
-// start the bot
-if (process.env.REPL_URL || process.env.HEROKU_URL) {
-  try {
-    await startWithWebhook(bot, token)
-  } catch (error) {
-    throw new Verror(error, 'Starting bot with webhook')
-  }
-  console.log('Bot is started webhook!')
-} else {
-  await bot.launch({ dropPendingUpdates: true })
-  console.log('Bot is started polling!')
-}
+})()
