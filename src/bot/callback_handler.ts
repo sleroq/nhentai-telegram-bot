c157850820d4ca8e6ef0f75bef6a1eed59482c28
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-22 23:57:17 +0300

    Update callback_handler.ts

--- src/bot/callback_handler.ts
+++ src/bot/callback_handler.ts
@@ -1,9 +1,10 @@
 import Verror           from 'verror'
 import { Context }      from 'telegraf'
-import { Update, CallbackQuery }       from 'telegraf/typings/core/types/typegram'
-import makeRandom       from './commands/random'
-import openInTelegraph  from './commands/open_in_telegraph'
-import likeDoujin       from './commands/like'
+import { Update, CallbackQuery } from 'telegraf/typings/core/types/typegram'
+import makeRandom         from './commands/random'
+import openInTelegraph    from './commands/open_in_telegraph'
+import likeDoujin         from './commands/like'
+import help, { editHelp } from './commands/help'
 
 export default async function callbackHandler(ctx: Context<Update>, callback_query: CallbackQuery.DataCallbackQuery): Promise<void> {
   const data: string = callback_query.data
@@ -11,25 +12,37 @@ export default async function callbackHandler(ctx: Context<Update>, callback_que
     try {
       await makeRandom(ctx,'next')
     } catch (error) {
-      throw new Verror(error, 'Random - \'next\' button')
+      throw new Verror(error, 'Handling Random:next button')
     }
   } else if (data === 'previous'){
     try {
       await makeRandom(ctx,'previous')
     } catch (error) {
-      throw new Verror(error, 'Random - \'previous\' button')
+      throw new Verror(error, 'Handling Random:previous button')
     }
   } else if (data.match('open')) {
     try {
       await openInTelegraph(ctx, data)
     } catch (error) {
-      throw new Verror(error, 'openInTelegraph')
+      throw new Verror(error, 'Handling openInTelegraph')
     }
   } else if (data.match('like_')){
     try {
       await likeDoujin(ctx, callback_query)
     } catch (error) {
-      throw new Verror(error, 'likeDoujin')
+      throw new Verror(error, 'Handling like')
+    }
+  } else if (data === 'searchtips') {
+    try {
+      await editHelp(ctx, callback_query)
+    } catch (error) {
+      throw new Verror(error, 'Showing search tips')
+    }
+  } else if (data === 'helpsearchback') {
+    try {
+      await help(ctx)
+    } catch (error) {
+      throw new Verror(error, 'Editing help')
     }
   }
 }
