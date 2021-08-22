309b8a16c325bb954f0c5021d76c9753e2fa4a50
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-22 23:48:33 +0300

    Update search.ts
    Fixed misspellings
    Fixed sorting for feature settings

--- src/bot/inline_search/search.ts
+++ src/bot/inline_search/search.ts
@@ -1,37 +1,38 @@
 import Context from 'telegraf/typings/context'
 import config  from '../../../config'
-import i18n 	 from '../../lib/i18n'
+import i18n    from '../../lib/i18n'
 import Verror  from 'verror'
 
 import { getMessageInline, sliceByHalf } from '../../lib/some_functions'
 
-import {
-  InlineQueryResultArticle,
-} 										from 'typegram'
-import { Document } 	from 'mongoose'
-import { UserSchema } from '../../models/user.model'
-import nHentai, { LightDoujin, SearchResult, SortingType } from '../../lib/nhentai'
-import {InlineQueryResult} from 'typegram/inline'
+import nHentai, {
+  LightDoujin,
+  SearchResult,
+  SortingType
+}                                   from '../../lib/nhentai'
+import { InlineQueryResultArticle } from 'typegram'
+import { User }               from '../../models/user.model'
+import { InlineQueryResult }          from 'typegram/inline'
 
 const nothingIsFoundResult: InlineQueryResultArticle = {
   id:                    String(6969696969),
   type:                  'article',
-  title:                 i18n.__('nothing_is_found'),
+  title:                 i18n.t('nothing_is_found'),
   description:           '',
   thumb_url:             config.help_icon_inline,
   input_message_content: {
-    message_text: i18n.__('help'),
+    message_text: i18n.t('help'),
     parse_mode:   'Markdown',
   },
   reply_markup: {
     inline_keyboard: [
       [
         {
-          text:          i18n.__('search_tips_button'),
+          text:          i18n.t('search_tips_button'),
           callback_data: 'searchtips',
         },
       ],
-      [{ text: i18n.__('settings_button'), callback_data: 'settings' }],
+      [{ text: i18n.t('settings_button'), callback_data: 'settings' }],
     ],
   },
 }
@@ -40,51 +41,69 @@ export default async function replyWithSearchInline(
   ctx: Context,
   inlineQuery: string,
   specifiedPage: number | undefined,
-  user: UserSchema & Document<any, any, UserSchema>
+  user: User
 ): Promise<void> {
   const searchType: 'photo' | 'article' = config.show_favorites_as_gallery ? 'photo' : 'article'
   
   const pageNumber = specifiedPage || 1
   inlineQuery = inlineQuery.replace(/\/p\d+/g, '').trim()
 
-  let sortingParametr: SortingType = user.search_sorting === 'popular' ? 'popular' : ''
+  let sortingParameter: SortingType = ''
   const matchSorting = inlineQuery.match(/\/s[pn]/)
   let isSearchModified = false
 
-  if (matchSorting) { // for example "@bot /sp smth"
+  if (user.search_sorting) {
+    switch (user.search_sorting) {
+    case 'popular':
+      sortingParameter = 'popular'
+      break
+    case 'popular-today':
+      sortingParameter = 'popular-today'
+      break
+    case 'popular-week':
+      sortingParameter = 'popular-week'
+      break
+    case 'date':
+    case 'new':
+      sortingParameter = ''
+      break
+    default:
+      console.error('Strange sorting parameter in user`s settings ' + user.search_sorting)
+    }
+  }
+  if (matchSorting) {       // for example "@bot /sp smth"
     isSearchModified = true // need this to add tips based on user's query
 
     switch (matchSorting[0].slice(2).trim()) {
     case 'p': 
-      sortingParametr = 'popular'
+      sortingParameter = 'popular'
       break
     case 'pt':
-      sortingParametr = 'popular-today'
+      sortingParameter = 'popular-today'
       break
     case 'pw':
-      sortingParametr = 'popular-week'
+      sortingParameter = 'popular-week'
       break
     case 'n':
-      break   
     case 'd':
+      sortingParameter = ''
       break
     default:
       console.error('Not allowed sorting \'' + matchSorting[0].slice(2).trim() + '\'')
     }
+
     inlineQuery = inlineQuery.replace(matchSorting[0], '').trim()
   }
 
   let searchResult: SearchResult
   try {
-    searchResult = await nHentai.search(inlineQuery, pageNumber, sortingParametr)
+    searchResult = await nHentai.search(inlineQuery, pageNumber, sortingParameter)
   } catch (error) {
     throw new Verror(error, 'Searching inline')
   }
   if (searchResult.totalSearchResults === 0){
-    const results: InlineQueryResultArticle[] = []
-    results.push(nothingIsFoundResult)
     try {
-      await ctx.answerInlineQuery(results).catch((err) => console.log(err))
+      await ctx.answerInlineQuery([nothingIsFoundResult])
     } catch (error) {
       throw new Verror(error, 'Answer Inline Nothing is found')
     }
@@ -92,7 +111,7 @@ export default async function replyWithSearchInline(
   }
 
   if (searchType === 'photo') {
-    const results: InlineQueryResult[] = await getResultsUniversal(user, searchResult.results, inlineQuery, isSearchModified, sortingParametr, pageNumber)
+    const results: InlineQueryResult[] = await getResultsUniversal(user, searchResult.results, inlineQuery, isSearchModified, sortingParameter, pageNumber)
     results.forEach((result)=>{
       result.type = 'photo'
     })
@@ -102,7 +121,7 @@ export default async function replyWithSearchInline(
       throw new Verror(error, 'Answer Inline Favorites Photo')
     }
   } else {
-    const results: InlineQueryResult[] = await getResultsUniversal(user, searchResult.results, inlineQuery, isSearchModified, sortingParametr, pageNumber)
+    const results: InlineQueryResult[] = await getResultsUniversal(user, searchResult.results, inlineQuery, isSearchModified, sortingParameter, pageNumber)
     results.forEach((result)=>{
       result.type = 'article'
     })
@@ -115,7 +134,7 @@ export default async function replyWithSearchInline(
 }
 
 async function getResultsUniversal(
-  user: UserSchema & Document<any, any, UserSchema>,
+  user: User,
   doujins: LightDoujin[],
   inlineQuery: string,
   isSearchModified: boolean,
@@ -163,7 +182,7 @@ async function getResultsUniversal(
       sortingParameter.includes('popular')
         ? config.sort_by_new_icon_inline
         : config.sort_by_popular_icon_inline,
-    sorting_tip_title = sortingParameter == 'popular' ? i18n.__('sorting_by_new_tip_title') : i18n.__('sorting_by_popularity_tip_title'),
+    sorting_tip_title = sortingParameter == 'popular' ? i18n.t('sorting_by_new_tip_title') : i18n.t('sorting_by_popularity_tip_title'),
     reverseSortingParametr = reverseSortingWord.charAt(0),
     searchSortingSwitch = pageNumber > 1
       ? `/p${pageNumber} /s${reverseSortingParametr} ${inlineQuery}`
@@ -173,7 +192,7 @@ async function getResultsUniversal(
     id:                    String(69696969420),
     type:                  'photo',
     title:                 sorting_tip_title,
-    description:           `Just add "/s${reverseSortingParametr}" to search qerry: (@nhentai_mangabot ${searchSortingSwitch})`,
+    description:           `Just add "/s${reverseSortingParametr}" to search query: (@nhentai_mangabot ${searchSortingSwitch})`,
     photo_url:             reverseSortingPhotoUrl,
     thumb_url:             reverseSortingPhotoUrl,
     input_message_content: {
@@ -203,20 +222,20 @@ async function getResultsUniversal(
   results.push({
     id:          String(9696969696),
     type:        'photo',
-    title:       i18n.__('next_page_tip_title'),
+    title:       i18n.t('next_page_tip_title'),
     description: `TAP HERE or Just add "/p${+pageNumber + 1
-    }" to search qerry: (@nhentai_mangabot ${nextPageSwitch})`,
+    }" to search query: (@nhentai_mangabot ${nextPageSwitch})`,
     photo_url:             config.next_page_icon_inline,
     thumb_url:             config.next_page_icon_inline,
     input_message_content: {
-      message_text: i18n.__('next_page_tip_message'),
+      message_text: i18n.t('next_page_tip_message'),
       parse_mode:   'HTML',
     },
     reply_markup: {
       inline_keyboard: [
         [
           {
-            text:                             i18n.__('next_page_button'),
+            text:                             i18n.t('next_page_button'),
             switch_inline_query_current_chat: nextPageSwitch,
           },
         ],
