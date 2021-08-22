0543de3013b829dca641dfcbc57c4d3053fa9915
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-22 23:43:57 +0300

    Finally fixed locales initialization

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,14 +1,30 @@
 import i18next from 'i18next'
-import Backend from 'i18next-fs-backend'
-import {join} from 'path'
+// import Backend from 'i18next-fs-backend'
+// import { join } from 'path'
+
+import en from '../locales/en/en'
+import ru from '../locales/ru/ru'
+import es from '../locales/es/es'
 
 i18next
-  .use(Backend)
+// .use(Backend)
   .init({
     fallbackLng: 'en',
-    load:        'all',
-    backend:     {
-      loadPath: join(__dirname, '../locales/{{lng}}/{{ns}}.js')
-    }
+    resources:   {
+      en: {
+        translation: en
+      },
+      ru: {
+        translation: ru
+      },
+      es: {
+        translation: es
+      }
+    },
+    load: 'all',
+    // backend:     {
+    //   loadPath: join(__dirname, '../locales/{{lng}}/{{ns}}.js')
+    // }
   })
-export default i18n
+
+export default i18next
