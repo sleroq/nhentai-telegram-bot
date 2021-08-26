73363e5ce4d476fef3d25842d40cf1d536c1fbf3
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-26 21:33:26 +0300

    Removed clearQueue function
    As I already have "drop_pending_updates" it is not necessary to run this function, especially since it gives an error if there was already a webhook before

--- src/express.ts
+++ src/express.ts
@@ -1,8 +1,6 @@
 import config from '../config'
 
 import express from 'express'
-import Verror from 'verror'
-import got from 'got'
 
 const expressApp = express()
 import api from './api.js'
@@ -10,19 +8,10 @@ import cors from 'cors'
 import { Context, Telegraf } from 'telegraf'
 import { Update } from 'telegraf/typings/core/types/typegram'
 
-async function clearQueue(token: string) {
-  try {
-    await got(`https://api.telegram.org/bot${token}/getUpdates?offset=-1`)
-  } catch (error) {
-    throw new Verror(error, 'Clearing queue by setting offset -1')
-  }
-}
 export default async function startWithWebhook(bot: Telegraf<Context<Update>>, token: string): Promise<void> {
   const port = process.env.PORT || 3000,
     domain = process.env.REPL_URL || process.env.HEROKU_URL
   const secretPath = `/telegraf/${Math.random().toString(36).substring(7)}`
-
-  await clearQueue(token)
 
   await bot.telegram.setWebhook(`${domain}${secretPath}`, {
     drop_pending_updates: true,
