b9771b0371916dd1278550e474f0dc81b1d47d80
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-28 20:32:03 +0300

    Rewritten

--- src/bot/commands/dlzip.ts
+++ src/bot/commands/dlzip.ts
@@ -1,97 +1,137 @@
-const nhentai = require("../../nhentai");
-const { saveAndGetUser } = require("../../db/save_and_get_user");
-const { getMangaMessage } = require("../some_functions.js");
-const fs = require("fs");
-const nhdl = require("../../nhentaidownloader");
+import Verror  from 'verror'
+import i18n    from '../../lib/i18n'
+import got, { Response } from 'got'
+import AdmZip  from 'adm-zip'
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-// there will be no comments here, I do not know how it works
-module.exports.dlzip = async function (ctx) {
-  let user = await saveAndGetUser(ctx);
-  let msg = ctx.message.text,
-    mangaId = msg.match(/\d+/) ? msg.match(/\d+/)[0] : null;
-  if (!mangaId) {
-    await ctx.reply(ctx.i18n.t("zip_tip"), {
-      parse_mode: "Markdown",
-    }).catch((err)=>{})
-    return;
-  }
-  let manga = await nhentai.getDoujin(mangaId).catch((err) => {
-    console.log(err.status);
-    return err.status
-  });
-  if (manga == 404) {
-    await ctx.reply(ctx.i18n.t("manga_does_not_exist") + "\n(`" + mangaId+ "`)",{
-      parse_mode: "Markdown",
-    })
-    return;
-  }
-  if (!manga) {
-    await ctx.reply(ctx.i18n.t("failed_to_get") + "\n(`" + mangaId + "`)", {
-      parse_mode: "Markdown",
-    });
-    return;
-  }
-  if (manga.details.pages > 150) {
-    await ctx.reply(ctx.i18n.t("too_many_pages"), {
-      parse_mode: "Markdown",
-    });
-    return;
-  }
-  await ctx.reply(ctx.i18n.t("waitabit_button"));
-  await ctx.replyWithChatAction("upload_document");
+import nhentai, { Doujin } from '../../lib/nhentai'
+import { getMangaMessage } from '../../lib/some_functions'
+import { Context }    from 'telegraf'
 
-  let messageText = getMangaMessage(manga, undefined, ctx.i18n);
+interface QueueItem { chatId: number | string, doujinId: number }
+let queue: QueueItem[] = []
 
-  await nhdl(mangaId).then((buffer) => {
-    fs.writeFileSync(`./${mangaId}.zip`, buffer);
-  }).catch(err=>{
-    console.log(err)
-    ctx.reply(ctx.i18n.t("something_went_wrong") + "\nError status: " + err.status, {
-      parse_mode: "Markdown",
-    });
-    return;
-  });
-  await ctx.replyWithChatAction("upload_document");
-  let stats = await fs.statSync(`./${mangaId}.zip`),
-    fileSizeB = stats["size"],
-    fileSizeMB = fileSizeB / 1048576.0;
-  console.log(fileSizeMB + " fileSizeMB");
-  console.log(fileSizeB + " fileSizeB");
-
-  if (fileSizeMB > 50) {
-    await ctx.reply(
-      ctx.i18n.t("file_is_too_big") + "\n" +
-      ctx.i18n.t("size_of_your_file") +
-      Math.round(fileSizeMB) + "mb",
-    );
-  } else {
-    await ctx.telegram
-      .sendDocument(
-        ctx.from.id,
-        {
-          source: fs.readFileSync(`./${mangaId}.zip`),
-          filename: `./${mangaId}.zip`,
-        },
-        {
-          caption: messageText,
-          parse_mode: "HTML",
-        }
-      )
-      .catch((err) => {
-        console.log("reply with file failed :( ", err);
-      });
-    await ctx.telegram
-      .deleteMessage(ctx.from.id, ctx.message.message_id + 1)
-      .catch((err) => console.log(err));
+let currentlyWorking = false
+export default async function dlZip(ctx: Context): Promise<void> {
+  if (!ctx.message || !('text' in ctx.message) || !ctx.message.text){
+    throw new Verror('No message text')
   }
-  await fs.unlink(`./${mangaId}.zip`, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`File ${mangaId}.zip deleted!`);
+  const message = ctx.message.text
+  const matchId = message.match(/\d+/)
+  const mangaId = matchId ? Number(matchId[0]) : null
+  
+  if (!mangaId || Number.isNaN(mangaId)) {
+    try {
+      await ctx.reply(i18n.t('zip_tip'), {
+        parse_mode: 'Markdown',
+      })
+    } catch (error) {
+      throw new Verror(error, 'Replying on /zip with no id')
     }
-  });
-};
+
+    return
+  }
+
+  queue.push({ chatId: ctx.message.chat.id, doujinId: mangaId })
+  if (!currentlyWorking) {
+    currentlyWorking = true
+    processQueue()
+  }
+
+  async function processQueue() {
+    try {
+      for (const {doujinId, chatId} of queue) {
+        let manga: Doujin | undefined
+        try {
+          manga = await nhentai.getDoujin(doujinId)
+        } catch (error) {
+          if (error.message === 'Not found') {
+            try {
+              await ctx.telegram.sendMessage(chatId, i18n.t('manga_does_not_exist') + ' (<code>' + doujinId + '</code>)')
+            } catch (error) {
+              removeFromQueue(queue, doujinId, chatId)
+              console.error('Replying \'404\': ' + error.message)
+            }
+          } else {
+            try {
+              await ctx.telegram.sendMessage(chatId, i18n.t('failed_to_get') + ' (<code>' + doujinId + '</code>)', {
+                parse_mode: 'HTML',
+              })
+            } catch (error) {
+              removeFromQueue(queue, doujinId, chatId)
+              console.error('Replying \'failed_to_get\': ' + error.message)
+            }
+          }
+          continue
+        }
+        if (manga.details.pages > 150) {
+          try {
+            await ctx.telegram.sendMessage(chatId, i18n.t('too_many_pages'), {parse_mode: 'HTML'})
+          } catch (error) {
+            removeFromQueue(queue, doujinId, chatId)
+            console.error('Replying /zip too many pages: ' + error.message)
+          }
+          continue
+        }
+
+        const messageText = getMangaMessage(manga)
+
+        const file = new AdmZip()
+
+        // TODO implement async
+        for (const page of manga.pages) {
+          let response: Response<Buffer> | undefined
+          try {
+            await ctx.telegram.sendChatAction(chatId, 'upload_document')
+          } catch (error) {
+            console.error('Making chat action "upload_document": ' + error.message)
+          }
+          try {
+            response = await got(page, {responseType: 'buffer'})
+          } catch (error) {
+            console.error('Downloading image ' + page + ': ' + error.message)
+            continue
+          }
+          file.addFile(manga.pages.indexOf(page) + '.jpg', response.body)
+        }
+        try {
+          await ctx.telegram.sendDocument(
+            chatId,
+            {
+              source:   file.toBuffer(),
+              filename: mangaId + '.zip'
+            },
+            {
+              caption:    messageText,
+              parse_mode: 'HTML',
+            }
+          )
+        } catch (error) {
+          queue = removeFromQueue(queue, doujinId, chatId)
+          if (error.code === 413) {
+            try {
+              await ctx.telegram.sendMessage(chatId, i18n.t('file_is_too_big'))
+            } catch (error) {
+              console.error('Replying with file_is_too_big: ' + error.message)
+            }
+            continue
+          }
+          console.error('Sending zip file ' + error.message)
+        }
+        queue = removeFromQueue(queue, doujinId, chatId)
+      }
+    } catch (error) {
+      console.error('Unexpected error processing queue:')
+      console.error(error)
+    }
+    if (queue.length !== 0) {
+      processQueue()
+    } else {
+      console.log('Finished work on /zip queue!')
+      currentlyWorking = false
+    }
+  }
+}
+
+function removeFromQueue(queue: QueueItem[], doujinId: number, chatId: number | string){
+  return queue.filter((item) => item.chatId !== chatId && item.doujinId !== doujinId)
+}
