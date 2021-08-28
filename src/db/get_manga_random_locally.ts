b7ddfaf3358bb42b6f80e7b343616fd47fe16dc5
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-08-28 22:30:03 +0300

    Refactoring

--- src/db/get_manga_random_locally.ts
+++ src/db/get_manga_random_locally.ts
@@ -1,35 +1,20 @@
-import Manga, { MangaSchema }    from '../models/manga.model'
-import { Document, FilterQuery } from 'mongoose'
+import MangaModel, { Manga } from '../models/manga.model'
+import { FilterQuery }       from 'mongoose'
 
-// TODO: fix ignore tags
 export default async function getRandomMangaLocally(
-  includedTags: string[] | undefined,
-  excludedTags: string[] | undefined
-): Promise<MangaSchema & Document<any, any, MangaSchema> | null> {
-  let query: FilterQuery<MangaSchema> | undefined
-
-  // const isExcluded = Array.isArray(excludedTags) && excludedTags[0]
-  // const isIncluded = Array.isArray(includedTags) && includedTags[0]
-  // if (isExcluded || isIncluded){
-  //   query = {
-  //     tags: {}
-  //   }
-  //   if (isIncluded){
-  //     query.tags.$in = includedTags
-  //   }
-  //   if (isExcluded) {
-  //     query.tags.$nin = excludedTags
-  //   }
-  // }
-
-  const count = await Manga.countDocuments()
-  if (count === 0 || count === null) {
-    return null
+  tags: string[] | undefined,
+  ninTags: string[] | undefined
+): Promise<Manga | null> {
+  const query: FilterQuery<typeof MangaModel> = { tags: { } }
+  if (tags?.length != 0 && query) {
+    query.tags.$in = tags
   }
-  const random = Math.floor(Math.random() * count)
-  const result = await Manga.findOne(query).skip(random)
-  if (!result) {
-    throw new Error('Could not get random doujin locally')
+  if (ninTags?.length != 0 && query) {
+    query.tags.$nin = ninTags
   }
-  return result
+
+  const count = await MangaModel.countDocuments(query),
+    random = Math.floor(Math.random() * count)
+
+  return MangaModel.findOne(query).skip(random)
 }
