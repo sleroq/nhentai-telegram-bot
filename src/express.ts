2443baeadf831ae1a00a44c99ea7d2fad391c94a
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-10-26 23:37:11 +0300

    Disabled api for now

--- src/express.ts
+++ src/express.ts
@@ -3,8 +3,8 @@ import config from '../config'
 import express from 'express'
 
 const expressApp = express()
-import api from './api.js'
-import cors from 'cors'
+// import Api from './api.js'
+// import cors from 'cors'
 import { Context, Telegraf } from 'telegraf'
 import { Update } from 'telegraf/typings/core/types/typegram'
 
@@ -22,42 +22,43 @@ export default async function startWithWebhook(bot: Telegraf<Context<Update>>, w
   expressApp.get('/', (req, res) => { res.send(config.express_get_slash) })
 
   // for api with statistics (./api.ts):
-  if (config.api_enabled) {
-    expressApp.use(cors())
+  // if (config.api_enabled) {
+  //   const api = new Api()
+  //   expressApp.use(cors())
 
-    expressApp.get('/api/countManga', async (req, res) => {
-      const answer = await api.countManga()
-      res.send(answer)
-    })
-    expressApp.get('/api/countUsers', async (req, res) => {
-      const answer = await api.countUsers()
-      res.send(answer)
-    })
-    expressApp.get('/api/countMessages', async (req, res) => {
-      const answer = await api.countMessages()
-      res.send(answer)
-    })
-    expressApp.get('/api/messagesToday', async (req, res) => {
-      const answer = await api.messagesToday()
-      res.send(answer)
-    })
-    expressApp.get('/api/mangaToday', async (req, res) => {
-      const answer = await api.mangaToday()
-      res.send(answer)
-    })
-    expressApp.get('/api/usersToday', async (req, res) => {
-      const answer = await api.usersToday()
-      res.send(answer)
-    })
-    expressApp.get('/api/lastManga', async (req, res) => {
-      const answer = await api.lastManga()
-      res.send(answer)
-    })
-    expressApp.get('/api', async (req, res) => {
-      const answer = await api.allInfo()
-      res.send(answer)
-    })
-  }
+  //   expressApp.get('/api/countManga', async (req, res) => {
+  //     const answer = await api.countManga()
+  //     res.send(answer)
+  //   })
+  //   expressApp.get('/api/countUsers', async (req, res) => {
+  //     const answer = await api.countUsers()
+  //     res.send(answer)
+  //   })
+  //   expressApp.get('/api/countMessages', async (req, res) => {
+  //     const answer = await api.countMessages()
+  //     res.send(answer)
+  //   })
+  //   expressApp.get('/api/messagesToday', async (req, res) => {
+  //     const answer = await api.messagesToday()
+  //     res.send(answer)
+  //   })
+  //   expressApp.get('/api/mangaToday', async (req, res) => {
+  //     const answer = await api.mangaToday()
+  //     res.send(answer)
+  //   })
+  //   expressApp.get('/api/usersToday', async (req, res) => {
+  //     const answer = await api.usersToday()
+  //     res.send(answer)
+  //   })
+  //   expressApp.get('/api/lastManga', async (req, res) => {
+  //     const answer = await api.lastManga()
+  //     res.send(answer)
+  //   })
+  //   expressApp.get('/api', async (req, res) => {
+  //     const answer = await api.allInfo()
+  //     res.send(answer)
+  //   })
+  // }
   expressApp.listen(port, () => {
     console.log(`Bot is running on port ${port}`)
   })
