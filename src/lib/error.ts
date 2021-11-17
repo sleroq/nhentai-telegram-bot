e0f51037c99c73146a3e0b484d62a369289b8d5e
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-17 21:52:33 +0300

    Fixed Werror class
    Fixed handling messages without error object

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -2,6 +2,8 @@ export default class Werror extends Error {
 	constructor(error: unknown, msg?: string) {
 		if (error instanceof Error) {
 			super(`${msg}: ${error.message}`)
+		} else if (typeof error === 'string') {
+			super(error)
 		} else {
 			console.error('error in Werror is not an instance of "Error"')
 			console.error(error)
