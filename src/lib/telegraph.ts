78adec1a63c8a353e7ddfe858c1fde88179f7def
Sleroq <cantundo@pm.me>
2022-03-20 01:03:48 +0300

    Removed telegra.ph dependency!

--- src/lib/telegraph.ts
+++ src/lib/telegraph.ts
@@ -1,56 +1,86 @@
-
 import Werror from './error'
 
-import Telegraph         from 'telegra.ph'
 import config            from '../../config'
 import got, { Response } from 'got'
 
 import { Doujin }      from './nhentai'
-import { Node, Page}   from 'telegra.ph/typings/telegraph'
 import { Manga }       from '../models/manga.model'
 import { getTitle }    from './some_functions'
 
 const token = process.env.TELEGRAPH_TOKEN
 
-let client: undefined | Telegraph
-
-function getClient(): Telegraph {
-	if (!token) {
-		throw new Error('No telegraph token')
-	}
-	if (!client) {
-		client = new Telegraph(token)
-	}
-	return client
+interface Page {
+	path: string
+	url: string
+	title: string
+	description: string
+	author_name?: string
+	author_url?: string
+	image_url?: string
+	content?: Array<Node>
+	views: number
+	can_edit?: boolean
 }
+
+interface NodeElement {
+	tag: 'a' | 'aside' | 'b' | 'blockquote' | 'br' | 'code' | 'em' | 'figcaption' | 'figure' | 'h3' | 'h4' | 'hr' | 'i' | 'iframe' | 'img' | 'li' | 'ol' | 'p' | 'pre' | 's' | 'strong' | 'u' | 'ul' | 'video'
+	attrs?: {
+		href?: string
+		src?: string
+	}
+	children?: Array<Node> | string
+}
+
+type Node = string | NodeElement
 
 export async function telegraphCreatePage(
 	manga: Doujin | Manga,
 	images: string[],
 	username = config.bot_username
 ): Promise<Page> {
-	const client = await getClient()
-	const page: Node[] = []
 	const title = getTitle(manga)
-	return client.createPage(
-		`${title}`,
-		page
-			.concat(
-				images.map((image) => ({
-					tag:   'img',
-					attrs: { src: `${image}` },
-				}))
-			)
-			.concat([
-				{
-					tag:      'a',
-					children: [config.text_at_the_end_of_telegraph_page],
-				},
-			]),
-		'@' + username,
-		'https://t.me/' + username,
-		true
-	)
+	const htmlImages: Node[] = images.map((image) => ({
+		tag:   'img',
+		attrs: { src: `${image}` },
+	}))
+
+	const content: Node[] = []
+	content.push(...htmlImages)
+
+	let contentString
+	try {
+		contentString = JSON.stringify(content)
+	} catch (error) {
+		throw new Werror(error, 'Stringifying content')
+	}
+
+	let response: Response<string>
+
+	try {
+		response = await got('https://api.telegra.ph/createPage', {
+			searchParams: {
+				access_token: token,
+				title: title,
+				author_name: '@' + username,
+				content: contentString,
+				return_content: true
+			}
+		})
+	} catch (error) {
+		throw new Werror(error, 'Making request')
+	}
+
+	let parsedResponse
+	try {
+		parsedResponse = JSON.parse(response.body)
+	} catch (error) {
+		throw new Werror(error, 'Parsing json response from telegra.ph')
+	}
+
+	if (!parsedResponse.ok) throw new Werror('Result is not ok: ' + parsedResponse.error)
+	if (!parsedResponse.result) throw new Werror('No result in response')
+
+	return parsedResponse.result
 }
 
 export default async function TelegraphUploadByUrls(
