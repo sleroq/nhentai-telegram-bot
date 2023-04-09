74954cffa54d9236fa904b30dbcaf5c97f87ccf0
Sleroq <hireme@sleroq.link>
2023-04-10 01:23:24 +0300

    Improved Error wrapper

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,12 +1,22 @@
-export default class Werror extends Error {
+class Wrap extends Error {
 	constructor(error: unknown, msg?: string) {
 		if (error instanceof Error) {
 			super(`${msg}: ${error.message}`)
 		} else if (typeof error === 'string') {
-			super(error)
+			super(`${msg}: ${error}`)
 		} else {
-			console.error('error in Werror is not an instance of "Error"')
-			console.error(error)
+			throw new Error('error in Werror is not an instance of "Error"')
+		}
+	}
+}
+
+export default class Werror extends Wrap {
+	cause?: Error
+	constructor(error: unknown, msg?: string) {
+		super(error, msg)
+
+		if (error instanceof Error) {
+			this.cause = error
 		}
 	}
 }
