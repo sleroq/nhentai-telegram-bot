e93d2b93648da63ace71d651778b1fa9a4da993d
Sleroq <hireme@sleroq.link>
2023-04-10 04:36:39 +0300

    Removed old sources

--- src/sources/e-hentai.test.ts
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-import { getAPIUrl } from './e-hentai.js'
-
-test('e-hentai APIUrl', async (t) => {
-	const apiUrl = getAPIUrl('https://e-hentai.org')
-	t.assert(apiUrl === 'https://api.e-hentai.org/api.php')
-})
